fe7e52c71ddd19bf7b8dfc000aed58923005ce75
Faruk <adekoladamilola4@gmail.com>
2022-10-13 17:38:22 +0100

    Refix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
-/**
-* Write a C program that prints exactly Programming is like building a
-* multilingual puzzle, followed by a new line.
-*/
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
