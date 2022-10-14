20ff43b1969dbca1b38d82f57a6e1f317c668df9
Faruk <adekoladamilola4@gmail.com>
2022-10-14 18:19:20 +0100

    Done with task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that print integer 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int integer;
+
+	for (integer = 0; integer <= 9; integer++)
+	{
+		printf(integer);
+	}
+	return (0);
+
+}
