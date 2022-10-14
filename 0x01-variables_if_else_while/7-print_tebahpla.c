cd75ae2d72fba3efe2b003e5c71809a588d5ef55
Faruk <adekoladamilola4@gmail.com>
2022-10-14 18:41:22 +0100

    Done with task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that print integer 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int integer;
+
+	for (integer = '9'; integer >= '0'; integer--)
+	{
+		putchar(integer);
+	}
+	putchar('\n');
+	return (0);
+
+}
