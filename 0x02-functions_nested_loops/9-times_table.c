fe104fe7f34c0fcae68965731adfd17268fb944d
Faruk <adekoladamilola4@gmail.com>
2022-10-18 17:48:10 +0100

    Refix task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,19 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, result;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			_putchar((i * j) + '0');
-			_putchar(',');
-			_putchar(' ');
+			result = i * j;
+			_putchar((result - '0');
+					if (j != 9)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 		}
 		_putchar('\n');
 	}
