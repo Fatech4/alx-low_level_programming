4a2edbd4f40870edec8f73b42a485cbcd12bd0a8
Faruk <adekoladamilola4@gmail.com>
2022-10-18 14:55:14 +0100

    Refix task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,7 @@ void print_alphabet_x10(void)
 	int count;
 	char letter;
 
-	for (count = 9; count <= 9; count++)
+	for (count = 1; count <= 10; count++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
