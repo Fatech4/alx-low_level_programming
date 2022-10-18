33ce3ea077e9186c4a60c02d08d4145b1d9ae7b8
Faruk <adekoladamilola4@gmail.com>
2022-10-18 18:04:17 +0100

    Refix task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,28 +1,39 @@
 #include "main.h"
-
 /**
- * times_table - A function that prints the 9 times table, starting with 0.
- *
- * Return: Return void
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
-
 void times_table(void)
 {
-	int i, j, result;
+	int i, j, d;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (j = 1; j <= 9; j++)
 		{
-			result = i * j;
-			_putchar((result - '0');
-					if (j != 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
+			d = (i * j);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
-}
 
+}
