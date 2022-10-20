6ebb1cb6d2e000a1d6c7b184bbe5c211ceb0b8d0
Faruk <adekoladamilola4@gmail.com>
2022-10-20 16:39:57 +0100

    Done with task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Main Entry point
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	int count;
+
+	for (count = 1; count <= 100; count++)
+	{
+		if (((count % 3) == 0) && ((count % 5) == 0))
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		else if ((count % 3) == 0)
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if ((count % 5) == 0)
+		{
+			printf("Buzz ");
+			continue;
+		}
+		printf("%d", count);
+		printf(" ");
+	}
+	return (0);
+}
