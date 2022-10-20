6deddd8bf4a28051ed1cc2012f0032aa6102e581
Faruk <adekoladamilola4@gmail.com>
2022-10-20 15:53:02 +0100

    Refix task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,12 +9,19 @@ void print_square(int size)
 {
 	int row, col;
 
-	for (row = 1; row <= size; row++)
+	if (size > 0)
 	{
-		for (col = 1; col <= size; col++)
+		for (row = 1; row <= size; row++)
 		{
-			_putchar('#');
+			for (col = 1; col <= size; col++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
