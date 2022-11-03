3381407d72a2f0482b7d527e271e675fd6bd2cb2
Faruk <adekoladamilola4@gmail.com>
2022-11-03 22:23:29 +0100

    Done with task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,53 @@
 #include "main.h"
-#include <stdio.h>
+int _strlen(char *str);
+int check_palindrome(int fwd, int bwd, char *str1);
 /**
  * is_palindrome - A function that returns 1 if a string is a palindrome and 0
  * if not
  * @s: String parameter
- * Return; Return 1 or 0
+ * Return: Return 1 or 0
  */
-//int is_palindrome(char *s)
-//{
 
-//}
-
-int main(void)
+int is_palindrome(char *s)
 {
-	printf("%s", reverse("Ade"));
-	return (0);
+	int j;
+
+	j = _strlen(s) - 1;
+	return (check_palindrome(0, j, s));
 }
-char *reverse(char *str, char * dst)
-{
-	if (*s == '\0')
-	{
-		return;
-	}
-	reverse((str + 1), dst);
-	
+/**
+ * _strlen - A function that return the length of a string
+ * @str: String parameter
+ * Return: The length of string
+ */
 
+int _strlen(char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(str + 1));
+}
+
+/**
+ * check_palindrome - A function that check for palindrome
+ * @fwd: Forward index
+ * @bwd: Backward index
+ * @str1: String parameter
+ * Return: 1 or 0
+ */
+
+int check_palindrome(int fwd, int bwd, char *str1)
+{
+
+	if (*(str1 + fwd) != *(str1 + bwd))
+	{
+		return (0);
+	}
+	if (fwd >= bwd)
+	{
+		return (1);
+	}
+	return (check_palindrome(fwd + 1, bwd - 1, str1));
 }
