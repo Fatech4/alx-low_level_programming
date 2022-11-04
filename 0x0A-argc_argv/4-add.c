9e1b0bed028b0db3bf747fbaac9f5d6a811406b0
Faruk <adekoladamilola4@gmail.com>
2022-11-04 22:03:14 +0100

    Refix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,12 +12,6 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
-	if (argc < 2)
-	{
-		printf("%d\n", 0);
-		return (0);
-	}
-
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) == 0)
