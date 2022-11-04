aaaa0f2ca70eb1f526db65927c07b4037ed23d01
Faruk <adekoladamilola4@gmail.com>
2022-11-04 21:12:07 +0100

    Refix

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -19,4 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 int _string_length(char *src);
-
+#endif
