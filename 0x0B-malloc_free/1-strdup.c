75b6f1608e4f9dafc7b3b822a3b1e38ae932664d
Faruk <adekoladamilola4@gmail.com>
2022-11-09 21:25:06 +0100

    Done with task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+int _strlen(char *str);
+/**
+ * _strdup - A function that returns a pointer to a newly allocated space in
+ * memory which contains a copy of the string given as a parameter
+ * @str: String parameter
+ * Return: Return a duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *s;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	s = malloc(_strlen(str));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < _strlen(str); i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+
+}
+/**
+ * _strlen - A function that count the characters in as string
+ * @str: String parameter
+ * Return: Return string length
+ */
+
+int _strlen(char *str)
+{
+	int i = 0;
+	int len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
