b047a78b87a944f7536b09cf161f945718be946b
Faruk <adekoladamilola4@gmail.com>
2022-11-10 22:20:57 +0100

    Refix task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,6 +14,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	int len_s2;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
 	len_s2 = _strlen(s2);
 
 	if ((int) n >= len_s2)
