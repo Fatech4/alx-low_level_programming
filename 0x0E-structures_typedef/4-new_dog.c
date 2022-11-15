0e36c18c4cd8cfada9c61478bc24117841aa6f83
Faruk <adekoladamilola4@gmail.com>
2022-11-15 23:38:36 +0100

    Refix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
-
+char *_strdup(char *str);
 /**
  * new_dog - A function that creates a new dog
  * @name: Name member
@@ -22,14 +21,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	else
 	{
-		my_dog->name = strdup(name);
+		my_dog->name = _strdup(name);
 		if (!my_dog->name)
 		{
 			free(my_dog);
 			return (NULL);
 		}
 		my_dog->age = age;
-		my_dog->owner = strdup(owner);
+		my_dog->owner = _strdup(owner);
 		if (!my_dog->owner)
 		{
 			free(my_dog->name);
@@ -38,4 +37,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 		}
 	}
 	return (my_dog);
+}
+/**
+ * *_strdup - function with one argument
+ * @str: string argument
+ *
+ * Description: returns a pointer to allocated space in memory
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		i++;
+	}
+
+	ptr = malloc(sizeof(char) * i + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	ptr[j] = '\0';
+	return (ptr);
 }
