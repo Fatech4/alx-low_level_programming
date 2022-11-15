124250bc33df207246ce5b4a107e29fde6fbe73b
Faruk <adekoladamilola4@gmail.com>
2022-11-15 23:01:48 +0100

    Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog 
+
+/**
+ * struct dog - Dog definition
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: Dog Definition
+ */
+
+struct dog
 {
 	char *name;
 	float age;
