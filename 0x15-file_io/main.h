29c7e7135e43e1138a8c86810354c57fc15c0e08
Faruk <adekoladamilola4@gmail.com>
2022-12-06 19:30:20 +0100

    Done with advanced task

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <elf.h>
 #define BUFSIZE 1024
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
