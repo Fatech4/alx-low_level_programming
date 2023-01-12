0e77449575c0aad8905e76b6208006604bdf6c11
Faruk <adekoladamilola4@gmail.com>
2023-01-12 23:33:30 +0100

    Refix task 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-
+unsigned int count_node(dlistint_t *head);
 /**
  * get_dnodeint_at_index - A function that returns the nth node of a
  * dlistint_t linked list
@@ -11,11 +11,41 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *arrow = head;
 	unsigned int count = 0;
+	unsigned int len = count_node(head);
 
-	while (count < index)
+	if (len == 0)
 	{
-		arrow = arrow->next;
-		count++;
+		return (NULL);
+	}
+	else if (index >= len)
+	{
+		return (NULL);
+	}
+	else
+	{
+		while (count < index)
+		{
+			arrow = arrow->next;
+			count++;
+		}
 	}
 	return (arrow);
+}
+
+/**
+ * count_node - Return numbers of node in a list
+ * @head: Head node
+ * Return: number of nodes
+ */
+unsigned int count_node(dlistint_t *head)
+{
+	int i = 0;
+	dlistint_t *arrow = head;
+
+	while (arrow != NULL)
+	{
+		i++;
+		arrow = arrow->next;
+	}
+	return (i);
 }
