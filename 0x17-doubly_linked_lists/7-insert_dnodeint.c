c619e14940d5ff6e0db1bb0ede31ca25db418b68
Faruk <adekoladamilola4@gmail.com>
2023-01-13 01:12:15 +0100

    Refix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,5 @@
 #include "lists.h"
-unsigned int count_node(dlistint_t **head);
+
 /**
  * insert_dnodeint_at_index - A function that inserts a new node at a given
  * @h: Head node
@@ -12,7 +12,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *arrow = *h;
 	dlistint_t *new;
 	unsigned int count = 0;
-	unsigned int len = count_node(h);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -27,45 +26,23 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (add_dnodeint(h, n));
 	}
-	else if (idx == (len - 1))
+	while (count < (idx - 1))
+	{
+		arrow = arrow->next;
+		count++;
+	}
+	if (arrow->next == NULL)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	else
+	new->n = n;
+	new->next = arrow->next;
+	arrow->next = new;
+	new->prev = arrow;
+	if (new->next != NULL)
 	{
-		while (count < (idx - 1))
-		{
-			arrow = arrow->next;
-			count++;
-		}
-		new->n = n;
-		new->next = arrow->next;
-		arrow->next = new;
-		new->prev = arrow;
-		if (new->next != NULL)
-		{
-			new->next->prev = new;
-		}
+		new->next->prev = new;
 	}
 	return (new);
 
-}
-
-
-/**
- * count_node - Return numbers of node in a list
- * @head: Head node
- * Return: number of nodes
- */
-unsigned int count_node(dlistint_t **head)
-{
-	int i = 0;
-	dlistint_t *arrow = *head;
-
-	while (arrow != NULL)
-	{
-		i++;
-		arrow = arrow->next;
-	}
-	return (i);
 }
